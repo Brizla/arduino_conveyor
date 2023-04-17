fd9225145c863190cc47b0ba78b8b5215aabf4a2
Bryn <elektrolyte@gmail.com>
2023-04-17 10:30:59 +1000

    Add files via upload
    this is the combined solution, prox switch + speed control + on/off switch ( connects Enable to Sleep)

--- stepperpot_speed_control.ino
+++ stepperpot_speed_control.ino
@@ -1,7 +1,9 @@
-//this one works 13/3/23
+//WORKS WITH PROX SWITCH! 17/4/23
 //adapted from https://forum.arduino.cc/t/stepper-motor-a4988-potentiometer-speed-control/654061/2
+//https://www.instructables.com/Project-on-E18-D80NK-IR-Proximity-Sensor-With-Ardu
 
 // Defines pins numbers
+const int e18_sensor = 7;
 const int stepPin = 6;
 const int dirPin = 5;
 const int enablePin = 4; 
@@ -9,7 +11,8 @@ int customDelay; // Defines variables
 int intervalBetweenSteps = 1;
  
 void setup() {
-  // Sets the three pins as Outputs
+  // Sets the sensor as input and three motor pins as Outputs
+  pinMode (e18_sensor, INPUT);
   pinMode(stepPin,OUTPUT);
   pinMode(dirPin,OUTPUT);
   pinMode(enablePin,OUTPUT);
@@ -18,23 +21,25 @@ void setup() {
   digitalWrite(enablePin,LOW); //Enables motor
 }
 void loop() {
- 
+int state = digitalRead(e18_sensor); // gets the sensor state
+if(state==LOW){
+digitalWrite(enablePin,HIGH);
+}
+else {
+digitalWrite(enablePin,LOW);
+} 
   intervalBetweenSteps = speedUp(); // Gets custom delay values from the custom speedUp function
   // Makes pulse with custom delay, depending on the Potentiometer, from which the speed of the motor depends
   digitalWrite(stepPin, HIGH);
   delayMicroseconds(100); // this is the pulse width
   digitalWrite(stepPin, LOW);
   delayMicroseconds(intervalBetweenSteps);
+
 }
 // Function for reading the Potentiometer
 int speedUp() {
   int customDelay = analogRead(A0); // Reads the potentiometer
-  if (customDelay > 1020) {
-    digitalWrite(enablePin, HIGH); //disables the motor
-  }
-  else {
-    digitalWrite(enablePin, LOW); //enables to motor
-  }
+
   int newCustom = map(customDelay, 0, 1020, 10,15000); // Converts the read values of the potentiometer from 0 to 1023 into desired delay values
   return newCustom;  
   
